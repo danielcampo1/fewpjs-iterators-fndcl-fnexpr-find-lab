b2f046772ba741234b8bf58c2a694f3965674221
github username <github email address>
2021-03-02 15:42:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function superbowlWin(record) {
+     let win = record.find(record =>record.result === 'W')
+      if (win){
+        return win.year
+     }
+    else {
+   return undefined
+    }
+    }
+
+
+
+// const inventory = [
+//     {name: 'apples', quantity: 2},
+//     {name: 'bananas', quantity: 0},
+//     {name: 'cherries', quantity: 5}
+//   ];
+  
+//   function isCherries(fruit) {
+//     return fruit.name === 'cherries';
+//   }
+  
+//   console.log(inventory.find(isCherries));
+//   // { name: 'cherries', quantity: 5 }
